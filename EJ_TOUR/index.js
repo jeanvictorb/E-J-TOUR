08c15b648580caeb0a133eb6467d20c4fb1c6f30
jeanvictorb <jeanvictor20032@gmail.com>
2025-02-01 12:35:51 -0300

    site

--- EJ_TOUR/index.js
+++ EJ_TOUR/index.js
@@ -2,3 +2,6 @@
 document.querySelector('.menu-toggle').addEventListener('click', () => {
     document.querySelector('.dropdown').classList.toggle('active');
 });
+
+document.getElementById('instagram-link').href = 'https://www.instagram.com/turismoej?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==';
+document.getElementById('whatsapp-link').href = 'https://api.whatsapp.com/send?phone=45999492697&text=Olá,%20gostaria%20de%20mais%20informações.';
