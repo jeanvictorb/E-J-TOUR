24d16c928eb0033fb65f76731375201bcbcf3f02
jean <jeanvictor20032@gmail.com>
2025-04-14 17:06:18 -0300

    new

---
+++ .angular/cache/18.2.15/exercicio2/vite/deps_ssr/@angular_platform-browser_animations.js
@@ -0,0 +1,23 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-WER2KXXD.js";
+import "./chunk-O2K6NUWL.js";
+import "./chunk-D3JV2RY4.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-NCYSEW5N.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
-import path from 'path';
-import { Component, NgModule } from '@angular/core';
 import { PrincipalComponent } from './components/layout/principal/principal.component';
-import { LoginComponent } from './components/layout/login/login.component';
 import { PacoteFromComponent } from './components/pacotes/pacote-from/pacote-from.component';
 import { PasseioFromComponent } from './components/passeio/passeio-from/passeio-from.component';
+import { IndexComponent } from './components/layout/index/index.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'principal', pathMatch: 'full'},
     {path: "principal", component: PrincipalComponent, children: [
+        {path: 'index', component: IndexComponent},
         {path: 'pacotes', component: PacoteFromComponent},
         {path: 'passeio', component: PasseioFromComponent}
 
