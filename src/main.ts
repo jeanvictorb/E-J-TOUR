dd8be3a4a2d6487dc7e54e146b4b4844f3df3df0
jean <jeanvictor20032@gmail.com>
2025-04-17 17:13:28 -0300

    analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
+import { APP_INITIALIZER, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { inject } from '@vercel/analytics';
 
-bootstrapApplication(AppComponent, appConfig)
+const analyticsInitializer = {
+  provide: APP_INITIALIZER,
+  useFactory: () => () => {
+    inject({ mode: isDevMode() ? 'development' : 'production' });
+  },
+  multi: true
+};
+
+const updatedAppConfig = {
+  ...appConfig,
+  providers: [...(appConfig.providers || []), analyticsInitializer]
+};
+
+bootstrapApplication(AppComponent, updatedAppConfig)
   .catch((err) => console.error(err));
